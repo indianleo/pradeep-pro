edc8dc4c3672358e52e5e712e60de017a1296699
Pradeep Yadav <pradeepdv45@gmail.com>
2017-11-07 12:30:02 +0530

    create products page in elements project

--- elements/index.php
+++ elements/index.php
@@ -25,7 +25,7 @@
               <a class="nav-link js-scroll-trigger" href="#services">Services</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link js-scroll-trigger" href="">Products</a>
+              <a class="nav-link js-scroll-trigger" href="products.php">Products</a>
             </li>
             <li class="nav-item">
               <a class="nav-link js-scroll-trigger" href="#portfolio">Portfolio</a>
@@ -71,7 +71,9 @@
               We provide most creative patterns and designer products which are comfortable and most reliable.
 
             </p>
-            <a class="btn btn-light btn-xl js-scroll-trigger" href="#services">Get Started!</a>
+            <a class="btn btn-light btn-xl js-scroll-trigger" href="#services">
+              Get Started
+            </a>
           </div>
         </div>
       </div>
@@ -220,34 +222,9 @@
     <section class="bg-dark text-white">
       <div class="container text-center">
         <h2 class="mb-4">Explore Products in Elements Interior</h2>
-        <a class="btn btn-light btn-xl sr-button" href="http://startbootstrap.com/template-overviews/creative/">Explore</a>
-      </div>
-    </section>
-
-    <section id="contact">
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-8 mx-auto text-center">
-            <h2 class="section-heading">Let's Get In Touch!</h2>
-            <hr class="my-4">
-            <p class="mb-5">
-               Ready to deliver product and want to pruchase our product ! Give Us a call or send us on an Email and We will get 
-               back to you as soon as possible.  
-            </p>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-lg-4 ml-auto text-center">
-            <i class="fa fa-phone fa-3x mb-3 sr-contact"></i>
-            <p>123-456-6789</p>
-          </div>
-          <div class="col-lg-4 mr-auto text-center">
-            <i class="fa fa-envelope-o fa-3x mb-3 sr-contact"></i>
-            <p>
-              <a href="mailto:your-email@your-domain.com">feedback@elementsinterio.com</a>
-            </p>
-          </div>
-        </div>
+        <a class="btn btn-light btn-xl sr-button" href="products.php">
+          Explore
+        </a>
       </div>
     </section>
 
---
+++ elements/products.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+
+    <?php include_once("header.html"); ?>
+
+  </head>
+
+  <body id="page-top">
+
+  		 <?php include_once("menu.html"); ?>
+
+  		 <header class="masthead text-center text-white d-flex">
+	      <div class="container my-auto">
+	        <div class="row">
+	          <div class="col-lg-10 mx-auto">
+	            <h1 class="text-uppercase">
+	              <strong>
+	                Elemtents Inerior has a lot of collections of Creativity
+	              </strong>
+	            </h1>
+	            <hr>
+	          </div>
+	          <div class="col-lg-8 mx-auto">
+	            <p class="text-faded mb-5">
+	              We provide wide range of home decoration, Fabric, Interiors, Architure according to you and our Products 
+	              having good qualities. 
+	            </p>
+	            <a class="btn btn-primary btn-xl js-scroll-trigger" href="#showcase">Explore Products</a>
+	          </div>
+	        </div>
+	      </div>
+	    </header>
+
+	    <section class="bg-primary" id="showcase">
+	      <div class="container">
+	        <h3>Here Products showcase will show....</h3>
+	      </div>
+	    </section>
+
+  	 	<?php include_once("footer.html"); ?>
+  </body>
+
+ </html>
