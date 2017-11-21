849a4bc03b2bd6f444c5f480dd95813146586b1b
Pradeep Yadav <pradeepdv45@gmail.com>
2017-11-21 11:33:01 +0530

    add services

---
+++ IIW/services.php
@@ -0,0 +1,261 @@
+<!doctype html>
+<html>
+    <head>
+        <?php include_once('header.html'); ?>
+        <link rel="stylesheet" type="text/css" href="css/services.css" />
+    </head>
+    <body>
+        <div class="menu_holder index_top">
+            <?php include_once('menu.html'); ?>
+        </div>
+        <div class="top_section" style="z-index:-1;">
+        </div>
+
+        <div class="service_box">
+            <div class="container">
+                <!--     Tiles Row Start -->
+                <div class="row">
+                    <div class="col-sm-4">
+                        <div class="tiles_item center vFlex bg_black">
+                            <div class="tiles_image row">
+                                <img 
+                                    src = "image/back4.jpg" 
+                                    class = "img-responsive" 
+                                    alt = "tiles images" 
+                                />
+                            </div>
+                            <div 
+                                class = "tiles_content text-center h_padding text_white" 
+                                style = "padding-top:15px;padding-bottom:50px;"
+                            >
+                                <span>
+                                    IIW is a plateform where you can explore information about things
+                                    which are arround you and your belongings. This is a place where
+                                    you can spread your business and find relevent things.
+                                </span> 
+                            </div>
+                            <div class="tiles_title text-center text_white" style="font-size:28px;">
+                                <span>
+                                    Information
+                                </span>
+                            </div>
+                            <div class="tiles_tag text-center" style="height:30px;color:#00cc99;">
+                                <span>
+                                    A plateform to know anything
+                                </span>
+                            </div>
+                            <div class="tiles_btn btn_style text_white" style="border:1px solid #e6e6e6;">
+                                <span>
+                                    <a href="search.php" class="text_white">
+                                        Explore
+                                    </a>
+                                </span>
+                            </div>
+                            <div style="height:50px;"></div>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-4">
+                        <div class="tiles_item center vFlex bg_black">
+                            <div class="tiles_image row">
+                                <img src="image/back4.jpg" class="img-responsive" alt="tiles images" />
+                            </div>
+                            <div 
+                                class = "tiles_content text-center h_padding text_white" 
+                                style = "padding-top:15px;padding-bottom:50px;"
+                            >
+                                <span>
+                                    We provide Search Engine Optimization for grow your business 
+                                    and networks. We help you to get to search ranking in every where 
+                                    and most of people will know you.
+                                </span> 
+                            </div>
+                            <div class="tiles_title text-center text_white" style="font-size:28px;">
+                                <span>
+                                    Seo Solution
+                                </span>
+                            </div>
+                            <div class="tiles_tag text-center" style="height:30px;color:#00cc99;">
+                                <span>
+                                    A place to grow your Bussines 
+                                </span>
+                            </div>
+                            <div class="tiles_btn btn_style text_white" style="border:1px solid #e6e6e6;">
+                                <span>
+                                    <a href="seo.php" class="text_white">
+                                        Explore
+                                    </a>
+                                </span>
+                            </div>
+                            <div style="height:50px;"></div>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-4">
+                        <div class="tiles_item center vFlex bg_black">
+                            <div class="tiles_image">
+                                <img src="image/back4.jpg" class="img-responsive" alt="tiles images" />
+                            </div>
+                            <div 
+                                class = "tiles_content text-center h_padding text_white" 
+                                style = "padding-top:15px;padding-bottom:50px;"
+                            >
+                                <span>
+                                    We provide Jobs according to your skills. Here you will find
+                                    most relevent job according to you desired location and we promise 
+                                    that you will never disappoint here.
+                                </span> 
+                            </div>
+                            <div class="tiles_title text-center text_white" style="font-size:28px;">
+                                <span>
+                                    Carrier Consultant
+                                </span>
+                            </div>
+                            <div class="tiles_tag text-center" style="height:30px;color:#00cc99;">
+                                <span>
+                                    An interface to find relevnt Job
+                                </span>
+                            </div>
+                            <div class="tiles_btn btn_style text_white" style="border:1px solid #e6e6e6;">
+                                <span>
+                                    <a href="job.php" class="text_white">
+                                        Explore
+                                    </a>
+                                </span>
+                            </div>
+                            <div style="height:50px;"></div>
+                        </div>
+
+                    </div>
+                    <!-- tiles row end -->
+
+                </div>
+                
+                <div class="row" style="padding-top:30px;">
+                    <div class="col-sm-4">
+                        <div class="tiles_item center vFlex bg_black">
+                            <div class="tiles_image row">
+                                <img 
+                                    src = "image/back4.jpg" 
+                                    class = "img-responsive" 
+                                    alt = "tiles images" 
+                                />
+                            </div>
+                            <div 
+                                class = "tiles_content text-center h_padding text_white" 
+                                style = "padding-top:15px;padding-bottom:50px;"
+                            >
+                                <span>
+                                    IIW is a plateform where you can explore latest News. 
+                                    Here you can create your own news and can share among all users. 
+                                    It can help to advertise your business.
+                                </span> 
+                            </div>
+                            <div class="tiles_title text-center text_white" style="font-size:28px;">
+                                <span>
+                                    News
+                                </span>
+                            </div>
+                            <div class="tiles_tag text-center" style="height:30px;color:#00cc99;">
+                                <span>
+                                    A place to read & Share News
+                                </span>
+                            </div>
+                            <div class="tiles_btn btn_style text_white" style="border:1px solid #e6e6e6;">
+                                <span>
+                                    <a href="news.php" class="text_white">
+                                        Explore
+                                    </a>
+                                </span>
+                            </div>
+                            <div style="height:50px;"></div>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-4">
+                        <div class="tiles_item center vFlex bg_black">
+                            <div class="tiles_image row">
+                                <img src="image/back4.jpg" class="img-responsive" alt="tiles images" />
+                            </div>
+                            <div 
+                                class = "tiles_content text-center h_padding text_white" 
+                                style = "padding-top:15px;padding-bottom:50px;"
+                            >
+                                <span>
+                                    We provide e-teaching and tutor. We provide coaching 
+                                    for C, C++, JavaScript, 
+                                    React Native(<span  style="color:#FF6C00;">IOS</span> & <span style="color:#FF6C00;">Android</span>),
+                                     React and new Technologies. 
+                                    Here you can get stuff for exam prepartion.
+                                </span> 
+                            </div>
+                            <div class="tiles_title text-center text_white" style="font-size:28px;">
+                                <span>
+                                    Education
+                                </span>
+                            </div>
+                            <div class="tiles_tag text-center" style="height:30px;color:#00cc99;">
+                                <span>
+                                    A place to learn 
+                                </span>
+                            </div>
+                            <div class="tiles_btn btn_style text_white" style="border:1px solid #e6e6e6;">
+                                <span>
+                                    <a href="edu.php" class="text_white">
+                                        Explore
+                                    </a>
+                                </span>
+                            </div>
+                            <div style="height:50px;"></div>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-4">
+                        <div class="tiles_item center vFlex bg_black">
+                            <div class="tiles_image">
+                                <img src="image/back4.jpg" class="img-responsive" alt="tiles images" />
+                            </div>
+                            <div 
+                                class = "tiles_content text-center h_padding text_white" 
+                                style = "padding-top:15px;padding-bottom:50px;"
+                            >
+                                <span>
+                                    We provide Jobs according to your skills. Here you will find
+                                    most relevent job according to you desired location and we promise 
+                                    that you will never disappoint here.
+                                </span> 
+                            </div>
+                            <div class="tiles_title text-center text_white" style="font-size:28px;">
+                                <span>
+                                   ...............
+                                </span>
+                            </div>
+                            <div class="tiles_tag text-center" style="height:30px;color:#00cc99;">
+                                <span>
+                                    Under Devlopement Services
+                                </span>
+                            </div>
+                            <div class="tiles_btn btn_style text_white" style="border:1px solid #e6e6e6;">
+                                <span>
+                                    <a href="share.php" class="text_white">
+                                        Explore
+                                    </a>
+                                </span>
+                            </div>
+                            <div style="height:50px;"></div>
+                        </div>
+
+                    </div>
+                    <!-- tiles row end -->
+
+                </div>
+
+            </div>
+        </div>
+
+        <div>
+            <?php include_once('footer.html'); ?>
+        </div>
+
+    </body>
+</html>
