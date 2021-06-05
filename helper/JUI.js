8f70aef368521cc8191eaa98052865950219eec8
Pradeep Yadav <pradeep.yadav@ucertify.com>
2021-06-05 14:09:50 +0530

    fixed

--- helper/JUI.js
+++ helper/JUI.js
@@ -1225,6 +1225,65 @@ export default class JUI extends API{
         }
     }
 
+    formatXml(xml, cdata_format) {
+        let cdata = cdata_format || false;
+        let reg = /(>)(<)(\/*)/g;
+        let wsexp = / *(.*) +\n/g;
+        let contexp = /(<.+>)(.+\n)/g;
+        let old_cdata = cdata ? xml.match(/<!--\[CDATA\[[\s\S]*?\]\]-->/gim) : "";
+        xml = xml.replace(/\t/g, '').replace(reg, '$1\n$2$3').replace(wsexp, '$1\n').replace(contexp, '$1\n$2');
+        if (cdata) {
+            let new_cdata = xml.match(/<!--\[CDATA\[[\s\S]*?\]\]-->/gim);
+            xml = xml.replace(new_cdata, old_cdata);
+        }
+        let formatted = '';
+        let lines = xml.split('\n');
+        let indent = 0;
+        let lastType = 'other';
+        // 4 types of tags - single, closing, opening, other (text, doctype, comment) - 4*4 = 16 transitions
+        let transitions = {
+            'single->single': 0,
+            'single->closing': -1,
+            'single->opening': 0,
+            'single->other': 0,
+            'closing->single': 0,
+            'closing->closing': -1,
+            'closing->opening': 0,
+            'closing->other': 0,
+            'opening->single': 1,
+            'opening->closing': 0,
+            'opening->opening': 1,
+            'opening->other': 1,
+            'other->single': 0,
+            'other->closing': -1,
+            'other->opening': 0,
+            'other->other': 0
+        };
+    
+        for (let i = 0; i < lines.length; i++) {
+            let ln = lines[i];
+            if (ln != '') {
+                let single = Boolean(ln.match(/<.+\/>/)); // is this line a single tag? ex. <br />
+                let closing = Boolean(ln.match(/<\/.+>/)); // is this a closing tag? ex. </a>
+                let opening = Boolean(ln.match(/<[^!].*>/)); // is this even a tag (that's not <!something>)
+                let type = single ? 'single' : closing ? 'closing' : opening ? 'opening' : 'other';
+                let fromTo = lastType + '->' + type;
+                lastType = type;
+                let padding = '';
+    
+                indent += transitions[fromTo];
+                for (let j = 0; j < indent; j++) {
+                    padding += '\t';
+                }
+                if (fromTo == 'opening->closing')
+                    formatted = formatted.substr(0, formatted.length - 1) + ln + '\n'; // substr removes line break (\n) from prev loop
+                else
+                    formatted += padding + ln + '\n';
+            }
+        }
+        return formatted;
+    }
+
     getModalHtml(data, type) {
         switch(type) {
             case 'Alert' : 
